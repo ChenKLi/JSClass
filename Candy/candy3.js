1bc87d0789fced59fa2cb5186184307eae302327
albert <albert3787@gmail.com>
2022-03-15 10:45:37 +0800

    add files candy1&4

--- Candy/candy3.js
+++ Candy/candy3.js
@@ -1,15 +1,17 @@
 let list = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
 
 function moveZerosToEnd(arr) {
-  let NewArray = [];
+  let newArray = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 0) {
-      // NewArray.push
+      newArray.push(arr[i]);
       arr.splice(i, 1);
     }
   }
-  return arr;
+  return arr.concat(newArray);
 }
 
 let result = moveZerosToEnd(list);
 console.log(result); // [false,1,1,2,1,3,"a",0,0]
+
+//用三個等號的方式過濾不是數字或不停資料類型的元素，然後將 0 push到新陣列後splice掉，再用concat新陣列合併就陣列得結果。
